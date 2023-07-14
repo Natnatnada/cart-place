bb6d81367313fa7be709f443afdfddc3469079fe
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-07-13 21:24:36 -0400

    adding useState for the filter

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ import { Products } from "./components/Product"
 import {products as initialProducts} from "./mocks/products.json"
 function App() {
 const [products]= useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+})  
+  const filterProducts = (products) => {
+    return products.filter(products => {
+      return (
+        products.price >= filters.minPrice &&
+        (
+          filters.category === 'all' || 
+       products.category ===filters.category   
+        )
+      )
+    })
+  }
+  const filteredProducts= filterProducts(products )
   return (
-   <Products products={products}/>
+    <Products products={filteredProducts}/>
   )
 }
 

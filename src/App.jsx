7d4adef021144a783234502d4ed969b16ee16017
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-07-26 22:09:57 -0400

    ft layour header for filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react"
 import { Products } from "./components/Product"
-import {products as initialProducts} from "./mocks/products.json"
+import { products as initialProducts } from "./mocks/products.json"
 function App() {
-const [products]= useState(initialProducts)
+  const [products] = useState(initialProducts)
   const [filters, setFilters] = useState({
     category: 'all',
     minPrice: 0
-})  
+  })
   const filterProducts = (products) => {
     return products.filter(products => {
       return (
         products.price >= filters.minPrice &&
         (
-          filters.category === 'all' || 
-       products.category ===filters.category   
+          filters.category === 'all' ||
+          products.category === filters.category
         )
       )
     })
   }
-  const filteredProducts= filterProducts(products )
+  const filteredProducts = filterProducts(products)
   return (
-    <Products products={filteredProducts}/>
+    <>
+      <Header/>
+      <Products products={filteredProducts} />
+    </>
   )
 }
 
